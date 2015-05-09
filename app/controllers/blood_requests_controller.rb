1dcfa67c011b37b2b6305faff8c9a0c0dad2d062
Sajani <deepasaj@thoughtworks.com>
2015-05-09 13:15:22 +0530

    Delete blood request

--- app/controllers/blood_requests_controller.rb
+++ app/controllers/blood_requests_controller.rb
@@ -6,4 +6,11 @@ class BloodRequestsController < ApplicationController
     requests_generated_by_other_users = all_requests.select{|req| req.user_id != user_id.to_i && req.active == true}
     render json: requests_generated_by_other_users
   end
+
+  def destroy
+    blood_request = BloodRequest.find(params[:id])
+    blood_request.update_attributes!(active: false)
+    blood_request.save
+    render json: {status: 'OK'}
+  end
 end
--- app/controllers/request_response_mappings_controller.rb
+++ app/controllers/request_response_mappings_controller.rb
@@ -1,9 +1,9 @@
 class RequestResponseMappingsController < ApplicationController
 
   def my_responses
-    active_request = BloodRequest.where(user_id: params[:user_id], active: true)
-    user_ids = RequestResponseMapping.where(blood_request_id: active_request.first.id)
+    active_request = BloodRequest.where(user_id: params[:user_id], active: true).first
+    user_ids = RequestResponseMapping.where(blood_request_id: active_request.id)
     responded_users = user_ids.collect { |i| User.find(i) }
-    render json: responded_users
+    render json: {active_request: active_request, responses: responded_users}
   end
 end
