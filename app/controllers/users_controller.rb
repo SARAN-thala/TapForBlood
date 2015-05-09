0e45e6ccbeec5597553a6086e2aded9518b2119e
Sajani <deepasaj@thoughtworks.com>
2015-05-09 10:59:54 +0530

    Create user end point

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    render json: {status: 'OK'}
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :phone_number, :blood_group, :area, :latitude, :longitude, :last_donated)
+  end
+end
