45d69aaa62f46850e75971fe72dcae4ad2f5d73c
Sajani <deepasaj@thoughtworks.com>
2015-05-09 18:34:28 +0530

    Create new blood request end point

--- app/controllers/blood_requests_controller.rb
+++ app/controllers/blood_requests_controller.rb
@@ -7,10 +7,22 @@ class BloodRequestsController < ApplicationController
     render json: requests_generated_by_other_users
   end
 
+  def create
+    @blood_request = BloodRequest.new(blood_request_params)
+    @blood_request.update_attributes!(active: true)
+    @blood_request.save
+    render json: {status: 'OK', blood_request_id: @blood_request.id}
+  end
+
   def destroy
     blood_request = BloodRequest.find(params[:id])
     blood_request.update_attributes!(active: false)
     blood_request.save
     render json: {status: 'OK'}
+  end
+
+  private
+  def blood_request_params
+    params.require(:blood_request).permit(:user_id, :blood_group, :area, :latitude, :longitude)
   end
 end
