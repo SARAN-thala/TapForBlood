e317959dffc8fccd567fbac41e176da6f271459b
Sajani <deepasaj@thoughtworks.com>
2015-05-10 10:15:59 +0530

    Twilio sms integration

--- app/controllers/blood_requests_controller.rb
+++ app/controllers/blood_requests_controller.rb
@@ -1,4 +1,5 @@
 require 'gcm'
+require 'twilio-ruby'
 class BloodRequestsController < ApplicationController
   include LocationHelper
 
@@ -17,11 +18,9 @@ class BloodRequestsController < ApplicationController
     @blood_request = BloodRequest.new(blood_request_params)
     @blood_request.update_attributes!(active: true)
     @blood_request.save
-    gcm = GCM.new("AIzaSyBFhjziCke8y5YXqFxzYhVQUpSOtnUT_eo")
-    registration_ids= get_neighbouring_device_ids @blood_request
-    options = {data: @blood_request}
-    response = gcm.send(registration_ids, options)
-    render json: {status: 'OK', blood_request_id: @blood_request.id, gcm_response: response}
+    gcm_response = gcm_notification @blood_request
+    sms_notification @blood_request
+    render json: {status: 'OK', blood_request_id: @blood_request.id, gcm_response: gcm_response}
   end
 
   def destroy
@@ -48,5 +47,39 @@ class BloodRequestsController < ApplicationController
       end
     }
     registration_ids
+  end
+
+  def get_neighbouring_phone_numbers(blood_request)
+    phone_numbers =[]
+    users = User.all
+    users = users.select{|user| user.id != blood_request.user_id}
+    users.each { |user|
+      distance = distance(user.latitude.to_f, user.longitude.to_f,
+                          blood_request.latitude.to_f, blood_request.longitude.to_f)
+      if distance < 100
+        phone_numbers.push(user.phone_number)
+      end
+    }
+    phone_numbers
+  end
+
+  def gcm_notification(blood_request)
+    gcm = GCM.new("AIzaSyBFhjziCke8y5YXqFxzYhVQUpSOtnUT_eo")
+    registration_ids= get_neighbouring_device_ids blood_request
+    options = {data: blood_request}
+    response = gcm.send(registration_ids, options)
+    response
+    end
+
+  def sms_notification(blood_request)
+    phone_numbers= get_neighbouring_phone_numbers blood_request
+    account_sid = 'ACe8903d205b720269cfdeeea07101c11e'
+    auth_token = '101e5eeb5b0add39110bc1130d7c2323'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    phone_numbers.each { |number|
+      @client.account.messages.create(:body => "1120-3608 Test Mess.. Please work please",
+                                                :to => number,:from => "+15005550006")
+    }
   end
 end
